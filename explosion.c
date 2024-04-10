1af40df89a27bb1d787dfb36631513b1df8d2f5c
Bugara <Bugara@DESKTOP-7IJ4MH3>
2024-04-10 19:23:10 +0200

    LOGIKA JEST CHYBA CAŁA

--- explosion.c
+++ explosion.c
@@ -7,14 +7,13 @@
 
 
 
-    // DZIALANIE
-// EKSPPLOZJA - BLOKI
-void explosionBlock(Bomb* bomb, Block** blocks) {
+void explosion(Bomb* bomb, Block** blocks, Player* players, int playerNumber) {
     int x = bomb->position.x;
     int y = bomb->position.y;
     int power = bomb->power;
 
-    bool destroyed = false; // flaga czy bylo zniszczenie
+    bool destroyed = false; // Flaga oznaczająca, czy bomba zniszczyła blok
+    bool damaged = false;   // Flaga oznaczająca, czy dany gracz został zraniony przez wybuch
 
     // Środek
     for (Block* blockElement = *blocks; blockElement != NULL && !destroyed; blockElement = blockElement->next) {
@@ -23,151 +22,127 @@ void explosionBlock(Bomb* bomb, Block** blocks) {
             if (blockElement->health == 0) {
                 breakBlock(blocks, blockElement);
             }
-            destroyed = true;
-            
+            destroyed = true; 
         }
     }
 
+    if (!destroyed) {
+        for (int i = 0; i < playerNumber; i++) {
+            if (players[i].position.x >= x - 20 && players[i].position.x <= x + 20 &&
+                players[i].position.y >= y - 20 && players[i].position.y <= y + 20) {
+                printf("\tSRODEK\n");
+                if (!damaged) { 
+                    players[i].health -= 1;
+                    damaged = true; 
+                }
+            }
+        }
+    }
 
-
-    destroyed = false;
     // Prawo
+    destroyed = false;
+    damaged = false;
+
     for (int i = 1; i <= power && !destroyed; i++) {
-        for (Block* blockElement = *blocks; blockElement != NULL; blockElement = blockElement->next) {
-            if (blockElement->position.x == x + 40 * i && blockElement->position.y == y) {
+        int newX = x + 40 * i;
+        for (Block* blockElement = *blocks; blockElement != NULL && !destroyed; blockElement = blockElement->next) {
+            if (blockElement->position.x == newX && blockElement->position.y == y) {
                 blockElement->health -= 1;
                 if (blockElement->health == 0) {
                     breakBlock(blocks, blockElement);
                 }
                 destroyed = true;
-                break;
+            }
+        }
+        if (!destroyed) {
+            for (int j = 0; j < playerNumber; j++) {
+                if (!damaged && players[j].position.y == y && players[j].position.x > x && players[j].position.x <= newX + 60) {
+                    players[j].health--;
+                    damaged = true;
+                    break;
+                }
             }
         }
     }
 
     // Lewo
     destroyed = false;
+    damaged = false;
+
     for (int i = 1; i <= power && !destroyed; i++) {
-        for (Block* blockElement = *blocks; blockElement != NULL; blockElement = blockElement->next) {
-            if (blockElement->position.x == x - 40 * i && blockElement->position.y == y) {
+        int newX = x - 40 * i;
+        for (Block* blockElement = *blocks; blockElement != NULL && !destroyed; blockElement = blockElement->next) {
+            if (blockElement->position.x == newX && blockElement->position.y == y) {
                 blockElement->health -= 1;
                 if (blockElement->health == 0) {
                     breakBlock(blocks, blockElement);
                 }
                 destroyed = true;
-                break;
+            }
+        }
+        if (!destroyed) {
+            for (int j = 0; j < playerNumber; j++) {
+                if (!damaged && players[j].position.y == y && players[j].position.x < x && players[j].position.x >= newX - 60) {
+                    players[j].health--;
+                    damaged = true;
+                    break;
+                }
             }
         }
     }
 
     // Góra
     destroyed = false;
+    damaged = false;
+
     for (int i = 1; i <= power && !destroyed; i++) {
-        for (Block* blockElement = *blocks; blockElement != NULL; blockElement = blockElement->next) {
-            if (blockElement->position.x == x && blockElement->position.y == y + 40 * i) {
+        int newY = y - 40 * i;
+        for (Block* blockElement = *blocks; blockElement != NULL && !destroyed; blockElement = blockElement->next) {
+            if (blockElement->position.x == x && blockElement->position.y == newY) {
                 blockElement->health -= 1;
                 if (blockElement->health == 0) {
                     breakBlock(blocks, blockElement);
                 }
                 destroyed = true;
-                break;
+            }
+        }
+        if (!destroyed) {
+            for (int j = 0; j < playerNumber; j++) {
+                if (!damaged && players[j].position.x == x && players[j].position.y < y && players[j].position.y >= newY - 60) {
+                    players[j].health--;
+                    damaged = true;
+                    break;
+                }
             }
         }
     }
 
     // Dół
     destroyed = false;
+    damaged = false;
+
     for (int i = 1; i <= power && !destroyed; i++) {
-        for (Block* blockElement = *blocks; blockElement != NULL; blockElement = blockElement->next) {
-            if (blockElement->position.x == x && blockElement->position.y == y - 40 * i) {
+        int newY = y + 40 * i;
+        for (Block* blockElement = *blocks; blockElement != NULL && !destroyed; blockElement = blockElement->next) {
+            if (blockElement->position.x == x && blockElement->position.y == newY) {
                 blockElement->health -= 1;
                 if (blockElement->health == 0) {
                     breakBlock(blocks, blockElement);
                 }
                 destroyed = true;
-                break;
             }
         }
-    }
-};
-
-// EKSPLOZJA - GRACZ
-
-
-void explosionPlayer(Bomb* bomb, Player* players, int playerNumber) {
-    int x = bomb->position.x;
-    int y = bomb->position.y;
-    int power = bomb->power;
-
-    bool damaged = false;
-
-    printf("%d %d\n", players[0].position.x, players[0].position.y);
-    // Srodek
-    
-    for (int i = 0; i < playerNumber; i++) {
-        if (!damaged && players[i].position.x >= x - 20 && players[i].position.x <= x + 20 &&
-            players[i].position.y >= y - 20 && players[i].position.y <= y + 20) {
-            printf("\tSRODEK\n");
-            players[i].health -= 1;
-            damaged = true;
-            break;
-        }
-    }
-    
-    // Prawo
-    damaged = false;
-    for (int i = 1; i <= power; i++) {
-        for (int j = 0; j < playerNumber; j++) {
-            if (!damaged && players[j].position.y == y && players[j].position.x > x && players[j].position.x <= x + 40 * i + 60) {
-                players[j].health -= 1;
-                damaged = true;
-                break;
+        if (!destroyed) {
+            for (int j = 0; j < playerNumber; j++) {
+                if (!damaged && players[j].position.x == x && players[j].position.y > y && players[j].position.y <= newY + 60) {
+                    players[j].health--;
+                    damaged = true;
+                    break;
+                }
             }
         }
     }
 
-    // Lewo
-    damaged = false;
-    for (int i = 1; i <= power; i++) {
-        for (int j = 0; j < playerNumber; j++) {
-            if (!damaged && players[j].position.y == y && players[j].position.x < x && players[j].position.x >= x - 40 * i - 60) {
-                players[j].health -= 1;
-                damaged = true;
-                break;
-            }
-        }
-    }
-
-    // DOL
-    damaged = false;
-    for (int i = 1; i <= power; i++) {
-        for (int j = 0; j < playerNumber; j++) {
-            if (!damaged && players[j].position.x == x && players[j].position.y > y && players[j].position.y <= y + 40 * i + 60) {            
-                players[j].health -= 1;
-            damaged = true;
-            break;
-            }
-        }
-    }
-
-    // GORA
-    damaged = false;
-    for (int i = 1; i <= power; i++) {
-        for (int j = 0; j < playerNumber; j++) {
-            if (!damaged && players[j].position.x == x && players[j].position.y < y && players[j].position.y >= y - 40 * i - 60) {
-                players[j].health -= 1;
-                damaged = true;
-                break;
-            }
-        }
-    }
-}
-
-
-
-void explosion(Bomb* bomb, Block** blocks, Player* players, int playerNumber) {
-    explosionBlock(bomb, blocks);
-    explosionPlayer(bomb, players, playerNumber);
-
-    
+    printf("\tH0: %d\n", players[0].health);
+    printf("\tH1: %d\n", players[1].health);
 }
